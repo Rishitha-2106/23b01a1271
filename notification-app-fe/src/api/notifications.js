ded6ba340b23ec984749fb69ba32b90b7f9ea56b
Rishitha <chowdaryrishitha804@gmail.com>
2026-06-25 14:13:56 +0530

    feat: add notification service

--- notification-app-fe/src/api/notifications.js
+++ notification-app-fe/src/api/notifications.js
@@ -1,2 +1,40 @@
-export function fetchNotifications() {
+import { Log } from "./logger";
+
+const BASE_URL = "http://4.224.186.213/evaluation-service";
+
+export async function getNotifications() {
+  const token = localStorage.getItem("accessToken");
+
+  try {
+    const response = await fetch(`${BASE_URL}/notifications`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Unable to fetch notifications");
+    }
+
+    const data = await response.json();
+
+    await Log(
+      "frontend",
+      "info",
+      "api",
+      "Notifications fetched successfully"
+    );
+
+    return data;
+  } catch (error) {
+    await Log(
+      "frontend",
+      "error",
+      "api",
+      error.message
+    );
+
+    throw error;
+  }
 }
